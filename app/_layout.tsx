79c307688497b2206242cf8edfb418b45a1eeebb
Prince Pal <princepal9120@gmail.com>
2025-02-19 19:15:25 +0530

    Add custom font loading and type definitions for images and data structures

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,28 @@
 
-import React from 'react'
-import { Stack } from 'expo-router'
+import React, { useEffect } from 'react'
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from 'expo-router'
 
 const RootLayout = () => {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false,}}/>
