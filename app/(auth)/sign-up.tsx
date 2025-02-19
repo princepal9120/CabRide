86eb06dd8946c8a72146d383c8889a851c3d8772
Prince Pal <princepal9120@gmail.com>
2025-02-19 23:40:46 +0530

    Enhance sign-up screen with InputField component for better styling and functionality

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,43 +1,61 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Alert, Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
-  const [form ,setForm] =useState({
-    name: '',
-    email: '',
-    password: '',
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
   });
+  const onSignUpPress = () => {
+    Alert.alert("Sign Up");
+  };
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
         <View className="realtive w-full h-[250px]">
           <Image source={images.signUpCar} className="w-full h-[250px] z-0" />
-          <Text className="text-center text-2xl font-semibold font-JakartaSemiBold absolute bottom-5 left-5 right-5">Create Your Account</Text>
+          <Text className="text-center text-2xl font-semibold font-JakartaSemiBold absolute bottom-5 left-5 right-5">
+            Create Your Account
+          </Text>
         </View>
         <View>
-          <InputField 
-          label="Name"
-          placeholder="Enter your name"
-          icon={icons.person}
-          value={form.name}
-          onChangeText={(value) => setForm({...form, name: value})}
+          <InputField
+            label="Name"
+            placeholder="Enter your name"
+            icon={icons.person}
+            value={form.name}
+            onChangeText={(value) => setForm({ ...form, name: value })}
           />
-          <InputField 
-          label="Email"
-          placeholder="Enter your email"
-          icon={icons.email}
-          value={form.email}
-          onChangeText={(value) => setForm({...form, email: value})}
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
           />
-          <InputField 
-          label="Password"
-          placeholder="Enter your password"
-          icon={icons.lock}
-          value={form.password}
-          onChangeText={(value) => setForm({...form, password: value})}
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
           />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+          {/* oauth */}
+
+          <Link href={'/sign-in'} className="mt-4 text-center text-general-200">
+          <Text>Already have an account?</Text>
+          <Text className="text-primary-500 underline"> Log In</Text>
+          </Link>
         </View>
       </View>
     </ScrollView>
--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,18 +1,58 @@
-import { View, Text, KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native'
-import React from 'react'
-import { InputFieldProps } from '@/types/type'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Image,
+  TextInput,
+  Platform,
+  Keyboard,
+} from "react-native";
+import React from "react";
+import { InputFieldProps } from "@/types/type";
 
-const InputField = ({label,labelStyle}: InputFieldProps) => {
+const InputField = ({
+  label,
+  labelStyle,
+  icon,
+  secureTextEntry = false,
+  containerStyle,
+  inputStyle,
+  iconStyle,
+  className,
+  ...props
+}: InputFieldProps) => {
   return (
-    <KeyboardAvoidingView>
-      <TouchableWithoutFeedback>
-        <View className='my-2 w-full'>
-<Text className={`text-lg font-JakartaSemiBold mb-3
-   ${labelStyle}`}>{label}</Text>
+    <KeyboardAvoidingView behavior={Platform.OS==="ios" ? "padding" : "height"}> 
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="my-2 w-full">
+          <Text
+            className={`text-lg font-JakartaSemiBold mb-3
+   ${labelStyle}`}
+          >
+            {label}
+          </Text>
+          <View
+            className={`flex flex-row items-center justify-start relative bg-neutral-100 rounded-full border-collapse
+          border-neutral-100 border-[1px]  focus:border-primary-500 ${containerStyle}
+            `}
+          >
+            {icon && (
+              <Image
+                source={icon}
+                className={`w-6 h-6 ml-4 ${iconStyle}`}
+              />
+         
+            )}
+                 <TextInput className={`rounded-full p-4 ${inputStyle} font-JakartaSemiBold text-[15px] flex-1 text-left`}
+                 secureTextEntry={secureTextEntry}
+                 {...props}
+                 />
+          </View>
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
